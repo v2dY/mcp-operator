52f954a42edef4e62d4f138800552f0325eebfb7
sarco3t <14gainward88@gmail.com>
2025-08-12 18:22:18 +0300

    bump generator version

--- internal/controller/resource_builder.go
+++ internal/controller/resource_builder.go
@@ -17,7 +17,7 @@ import (
 )
 
 const (
-	newGenImage   = "ghcr.io/v2dy/mcp-gen:0.1.1"
+	newGenImage   = "ghcr.io/v2dy/mcp-gen:0.1.2"
 	PrebuiltImage = "sarco3t/openapi-mcp-generator:1.0.6"
 	ContainerPort = 3001
 )
